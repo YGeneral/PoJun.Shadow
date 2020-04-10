10ab6ae4fd21a3f8a377723743cdd1caa8f2083c
杨江军 <jiangjun.yang@ashermed.com>
2020-04-10 17:11:47 +0800

    增加泛型集合去重工具类

---
+++ source/PoJun.Shadow.Shared/PoJun.Shadow.Tools/Base/FastPropertyComparer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace PoJun.Shadow.Tools.Base
+{
+    /// <summary>
+	/// 集合去重快速比较器
+    /// 使用方法：param.UserInfos.Distinct(new FastPropertyComparer 尖括号 实体类名 尖括号("需要去重的字段名")).ToList()
+	/// 创建人：杨江军
+	/// 创建时间：2020/4/10 16:27:59
+	/// </summary>
+	public class FastPropertyComparer<T> : IEqualityComparer<T>
+    {
+        private Func<T, Object> getPropertyValueFunc = null;
+
+        /// <summary>
+        /// 通过propertyName 获取PropertyInfo对象
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public FastPropertyComparer(string propertyName)
+        {
+            PropertyInfo _PropertyInfo = typeof(T).GetProperty(propertyName,
+            BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.Public);
+            if (_PropertyInfo == null)
+            {
+                throw new ArgumentException(string.Format("{0} is not a property of type {1}.",
+                    propertyName, typeof(T)));
+            }
+
+            ParameterExpression expPara = Expression.Parameter(typeof(T), "obj");
+            MemberExpression me = Expression.Property(expPara, _PropertyInfo);
+            getPropertyValueFunc = Expression.Lambda<Func<T, object>>(me, expPara).Compile();
+        }
+
+        #region IEqualityComparer<T> Members
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool Equals(T x, T y)
+        {
+            object xValue = getPropertyValueFunc(x);
+            object yValue = getPropertyValueFunc(y);
+
+            if (xValue == null)
+                return yValue == null;
+
+            return xValue.Equals(yValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int GetHashCode(T obj)
+        {
+            object propertyValue = getPropertyValueFunc(obj);
+
+            if (propertyValue == null)
+                return 0;
+            else
+                return propertyValue.GetHashCode();
+        }
+
+        #endregion
+    }
+}
